b9862b552ec576c2cf39f6f17ff738928da6ee77
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-06-23 18:48:49 +0900

    Feat : 첫번째 리뷰 포인트 획득
    유저가 장소의 첫번째 리뷰를 썼다면 1점 획득

---
+++ src/main/java/com/sunggwang/triple/domain/place/entity/Place.java
@@ -0,0 +1,28 @@
+package com.sunggwang.triple.domain.place.entity;
+
+import com.sunggwang.triple.config.BaseTimeEnity;
+import com.sunggwang.triple.domain.user.entity.User;
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Place extends BaseTimeEnity {
+
+    @Id
+    private UUID id;
+
+    private Boolean isFirstReview = Boolean.TRUE;
+
+    public void checkFirstReview(User user) {
+        if (this.isFirstReview) {
+            user.getFirstReviewPoint();
+        }
+    }
+}
