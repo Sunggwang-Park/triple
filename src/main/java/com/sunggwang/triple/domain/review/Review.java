f4383b3ac4f7684d112d57cef8d420aa2c34adb6
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-06-23 18:58:20 +0900

    Feat : 리뷰 도메인 생성
    User, Place 다대일 매핑 (레이지 로딩)

---
+++ src/main/java/com/sunggwang/triple/domain/review/Review.java
@@ -0,0 +1,32 @@
+package com.sunggwang.triple.domain.review;
+
+import com.sunggwang.triple.config.BaseTimeEnity;
+import com.sunggwang.triple.domain.place.entity.Place;
+import com.sunggwang.triple.domain.user.entity.User;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.print.DocFlavor;
+import javax.swing.*;
+import java.util.Stack;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review extends BaseTimeEnity {
+
+    @Id
+    private UUID id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Place place;
+
+}
