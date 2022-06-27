74759592d79e883f05c2073546a5259db3566180
Sunggwang Park <54604123+Sunggwang-Park@users.noreply.github.com>
2022-06-27 18:30:23 +0900

    Feat : 이벤트 컨트롤러

---
+++ src/main/java/com/sunggwang/triple/controller/EventController.java
@@ -0,0 +1,29 @@
+package com.sunggwang.triple.controller;
+
+import com.sunggwang.triple.domain.review.dto.ReviewEventRequestDto;
+import com.sunggwang.triple.domain.review.service.ReviewService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/events")
+public class EventController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("")
+    public void event(ReviewEventRequestDto dto) {
+        if (dto.getType().equals("REVIEW")) {
+            if (dto.getAction().equals("DELETE")) {
+                reviewService.lostPoints(dto);
+            } else {
+                reviewService.getPoints(dto);
+            }
+        }
+    }
+
+
+}
